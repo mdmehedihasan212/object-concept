e928def6a2559797c21532d28cf1d03f3edbe57b
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-02-26 12:22:48 +0600

    object declare system

--- objecte-declare.js
+++ objecte-declare.js
@@ -9,3 +9,30 @@ console.log(ringku);
 // inheritance
 const guru = Object.create(panku)
 console.log(guru.age);
+
+// classical object declare
+class people {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+const peo = new people('tom hulk', 21)
+console.log(peo);
+
+class human {
+    constructor(body, hand) {
+        this.body = body;
+        this.hand = hand;
+    }
+}
+const suruj = new human('black', 2)
+console.log(suruj);
+
+// functional object declare syntactical sugar
+
+function Total(hight) {
+    this.hight = hight;
+}
+const properti = new Total(6);
+console.log(properti);
