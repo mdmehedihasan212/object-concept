707753f044317fe777a5a890a4779503914d69e9
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-02-26 11:44:38 +0600

    first commit

---
+++ objecte-declare.js
@@ -0,0 +1,7 @@
+// using literal
+const panku = { names: 'jan pakhi', age: 30, status: 'love' }
+console.log(panku.age);
+
+// using constructor
+const ringku = new Object('dada')
+console.log(ringku);
